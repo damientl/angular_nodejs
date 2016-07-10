77dffaaaffd70c0543fdf8565df04ff4cdb7f373
Damien Lutz <damien.lutz@ilegra.com>
2016-07-10 19:21:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular_nodejs
+node
---
+++ node_app/model/Restaurant.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = class Restaurant {
+
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
